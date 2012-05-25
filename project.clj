02d15fdb528c410b0b3041c41046b2c17c1382fc
Aleksander Skjæveland Larsen <aleksander@ogrim.no>
2012-05-25 09:43:51 +0200

    Fikset databasehåndtering, setter kun inn unike hendelser

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject kanskje-kommer-kongen "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "https://github.com/ogrim"
+  :description "Hvor er kongen?"
+  :url "https://github.com/ogrim/kanskje-kommer-kongen"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
--- src/kanskje_kommer_kongen/database.clj
+++ src/kanskje_kommer_kongen/database.clj
@@ -3,14 +3,14 @@
             [clj-time [core :as t] [coerce :as c]])
   (:use [korma core db]))
 
-(def dbspec
+(def ^:private dbspec
   {:classname   "org.sqlite.JDBC"
    :subprotocol "sqlite"
    :subname     "kongen.db"})
 
 (defdb db dbspec)
 
-(defn create-tables []
+(defn- create-tables []
   (do
     (sql/create-table "program"
                       [:tittel :text]
@@ -19,38 +19,34 @@
                       [:klokkeslett :text]
                       [:fra :datetime]
                       [:til :datetime]
-                      ["PRIMARY KEY" "(tittel, dato)"])
+                      ["PRIMARY KEY" "(tittel, dato, klokkeslett)"])
     (sql/do-commands "CREATE INDEX idx_program_tittel ON program(tittel)"
-                     "CREATE INDEX idx_program_dato ON program(dato)")))
+                     "CREATE INDEX idx_program_dato ON program(dato)"
+                     "CREATE INDEX idx_program_klokkeslett ON program(klokkeslett)")))
 
-(defn invoke-with-connection [f]
+(defn- invoke-with-connection [f]
   (sql/with-connection dbspec (sql/transaction (f))))
 
 (try (invoke-with-connection create-tables) (catch Exception _ nil))
-
-(defn timestamp []
-  (java.sql.Timestamp. (.getTime (java.util.Date.))))
-
-(comment (join query my-ent (and (= :k1 :s.k1)
-                         (= :k2 :s.k2))))
 
 (defentity program
   (table :program)
   (entity-fields :tittel :tekst :dato :klokkeslett :fra :til))
 
+(defn- can-insert? [tittel dato klokkeslett]
+  (empty? (select program
+            (where {:tittel tittel
+                    :dato dato
+                    :klokkeslett klokkeslett}))))
+
 (defn insert-program [{:keys [dato klokkeslett dato-intervall tittel tekst]}]
   (let [fra (if (nil? dato-intervall) nil (c/to-timestamp (t/start dato-intervall)))
-        til (if (nil? dato-intervall) nil (c/to-timestamp (t/end dato-intervall)))]
-    (insert program (values {:tittel tittel
-                             :tekst tekst
-                             :dato (c/to-timestamp dato)
-                             :klokkeslett klokkeslett
-                             :fra fra
-                             :til til}))))
-
-(comment (defn get-page [url]
-   (let [[query] (select websites (where {:url url}))]
-     (if (empty? query)
-       (do (persist-page url) (get-page url))
-       {:html (read-string (:html query))
-        :status (:status query)}))))
+        til (if (nil? dato-intervall) nil (c/to-timestamp (t/end dato-intervall)))
+        sql-dato (c/to-timestamp dato)]
+    (if (can-insert? tittel sql-dato klokkeslett)
+      (insert program (values {:tittel tittel
+                               :tekst tekst
+                               :dato sql-dato
+                               :klokkeslett klokkeslett
+                               :fra fra
+                               :til til})))))
